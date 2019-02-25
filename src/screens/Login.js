de42b9fc1c6f95527268ff4a5052db6fb77a9077
Shirline Serrao <shirlineserrao@Shirlines-MacBook-Air.local>
2019-02-25 10:36:49 +0000

    SignUp and Login form

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, TouchableHighlight } from 'react-native';
 import t from 'tcomb-form-native';
+import { validate } from 'tcomb-validation';
 
 const Form = t.form.Form;
 
@@ -8,26 +9,91 @@ const User = t.struct({
   email: t.String,
   username: t.String,
   password: t.String,
-  terms: t.Boolean
 });
 
+const options = {
+  fields: {
+    email: {
+      error: 'Enter a valid email address'
+    },
+    password: {
+      error: 'Password is required'
+    },
+  },
+  stylesheet: formStyles,
+};
+
 export default class App extends Component {
-    render() {
-      return (
-        <View style={styles.container}>
-        <Text>Login Page</Text>
-          {/* <Form type={User} /> Notice the addition of the Form component */}
-        </View>
-      );
+  
+  handleSubmit = () => {
+    const value = this._form.getValue();
+    console.log('value: ', value);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Form 
+          ref={c => this._form = c}
+          type={User} 
+          options={options}
+          // value={this.state.value}
+          // onChange={this.onChange}
+        />
+        {/* <Button
+          style={styles.button}
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        /> */}
+        <TouchableHighlight style={styles.button} onPress={this.handleSignUp} underlayColor='#99d9f4' onPress={this.handleSubmit}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+  
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+});
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: 'blue',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
     }
   }
-
-  
-  const styles = StyleSheet.create({
-    container: {
-      justifyContent: 'center',
-      marginTop: 50,
-      padding: 20,
-      backgroundColor: '#ffffff',
-    },
-  });
+}
